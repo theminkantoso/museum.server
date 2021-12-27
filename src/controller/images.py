428827ed4638fd9b1263c7ed3f7330bb25b86786
tuyenshin2004 <tuyenshin2004@gmail.com>
2021-12-27 23:34:53 +0700

    commit

--- src/controller/images.py
+++ src/controller/images.py
@@ -26,26 +26,26 @@ class image(Resource):
             # print(type(b))
             # return Response(img.Url, mimetype=img.MimeType)
             return img.json()
-        return {'msg': 'Image not found'}, 404
+        return {'message': 'Image not found'}, 404
 
     def post(self):
         pic = request.files['pic']
         if not pic:
-            return {'msg': 'No pic loader'}, 400
+            return {'message': 'No pic loader'}, 400
         filename = secure_filename(pic.filename)
         mimetype = pic.mimetype
         url = filename
         path = 'statics/images/' + filename
 
         if Image.find_by_name(filename):
-            return {'msg': "An image with name '{}' already exists.".format(filename)}, 400
+            return {'message': "An image with name '{}' already exists.".format(filename)}, 400
         # img = Image(Name=filename, Content='', Url=pic.read(), Path='', MimeType=mimetype)
         pic.save(os.path.join(APP_ROOT, filename))
         img = Image(Name=filename, Content='', Url=url, Path=path, MimeType=mimetype)
         try:
             img.save_to_db()
         except:
-            return {"msg": "An error occurred inserting the image."}, 500
+            return {"message": "An error occurred inserting the image."}, 500
         img = Image.find_by_name(filename)
         return {'msg': 'Image added.', 'ImageId': img.ImageId}, 200
 
@@ -53,7 +53,7 @@ class image(Resource):
         img = Image.find_by_id(id)
         if img:
             img.delete_from_db()
-            return {'msg': 'Image deleted.'}
+            return {'message': 'Image deleted.'}
         return {'msg': 'Image not found.'}, 404
 
     def put(self, id):
@@ -65,8 +65,8 @@ class image(Resource):
             img.Url = data['Url']
             img.Path = data['Path']
             img.save_to_db()
-            return {'msg': 'Image put'}
-        return {'msg': 'Image not found.'}, 404
+            return {'message': 'Image put'}
+        return {'message': 'Image not found.'}, 404
 
 
 class Images(Resource):
