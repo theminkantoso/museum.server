d2ca18ebd7b16ed9766210e9a0cef7501b2aceb3
tuyenshin2004 <tuyenshin2004@gmail.com>
2021-12-29 00:44:33 +0700

    commit notification

--- src/controller/notification.py
+++ src/controller/notification.py
@@ -1,6 +1,6 @@
 import re
 from datetime import datetime
-
+from flask_jwt_extended import get_jwt_identity
 
 from src.models.accountDb import AccountDb
 
@@ -29,8 +29,8 @@ class notification(Resource):
         return {'message': ' not found.'}, 404
 
     def post(self):
+        id_acc = get_jwt_identity()
         parser = reqparse.RequestParser()
-        parser.add_argument('AccountId', type=int)
         parser.add_argument('Title', type=str)
         parser.add_argument('Content', type=str)
         data = parser.parse_args()
@@ -38,7 +38,7 @@ class notification(Resource):
         if not validate_regex(str(data["AccountId"]), regex_id):
             return {"message": "invalid AccountId"}, 400
         timeNow = datetime.now()
-        r = Notification(AccountId=data["AccountId"], Title=data["Title"],
+        r = Notification(AccountId=id_acc, Title=data["Title"],
                          Content=data["Content"], Time=timeNow, Unread=0)
         try:
             r.save_to_db()
@@ -99,10 +99,11 @@ class NotificationsAll(Resource):
 
 
 class Notifications(Resource):  # tất cả thông báo của mỗi người dùng.
-    def get(self, AccId):
-        if not validate_regex(AccId, regex_id):
+    def get(self):
+        id_acc = get_jwt_identity()
+        if not validate_regex(id_acc, regex_id):
             return {"message": "invalid AccountId"}, 400
-        data = {'notifications': list(map(lambda x: x.json(), Notification.find_All_Notifications_by_AccId(AccId)))}
+        data = {'notifications': list(map(lambda x: x.json(), Notification.find_All_Notifications_by_AccId(id_acc)))}
         return data, 200
 
 #  post 1 thông báo => lưu cho tất cả người dùng (Admin) => Không cần gửi id người dùng
